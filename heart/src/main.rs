728bf0b785c1d72e2d2518d3d60777c046f0ff80
theMaimu <barmettler.lars@gmail.com>
2023-11-30 10:35:59 +0100

    add tests

--- heart/src/main.rs
+++ heart/src/main.rs
@@ -4,6 +4,7 @@ use std::time::Instant;
 
 use tenaciouszebra_singe_rocksdb::database::{TableTransaction, Database};
 use tenaciouszebra_file_store::database::{TableTransaction as FileStoreTableTransaction, Database as FileStoreDatabase};
+use tenaciouszebra_dashmap::database::{TableTransaction as DashMapTableTransaction, Database as DashMapDatabase};
 use crate::helpers::with_percentage_true;
 
 mod commands;
@@ -49,7 +50,7 @@ fn run_single_rocksdb_test(write_percentage: i32, transaction_size: usize, trans
             if with_percentage_true(write_percentage) {
                 modify.set(format!("{}{}", i, j), j).unwrap();
             } else {
-                modify.get(&format!("{}{}", i, j)).unwrap();
+                modify.get(format!("{}{}", i, j)).unwrap();
             }
         }
         transactions.push(modify);
@@ -73,9 +74,7 @@ fn run_single_rocksdb_test(write_percentage: i32, transaction_size: usize, trans
 
 fn run_no_backup_test(write_percentage: i32, transaction_size: usize, transaction_count: usize) -> u128 {
     let db= FileStoreDatabase::<String, usize>::new();
-    let mut test_table = db.empty_table("test");
 
-    let start = Instant::now();
     let mut transactions = Vec::<FileStoreTableTransaction<String, usize>>::new();
 
     for i in 0..transaction_count {
@@ -103,6 +102,35 @@ fn run_no_backup_test(write_percentage: i32, transaction_size: usize, transactio
     duration.as_millis()
 }
 
+fn run_no_backup_dashmap_test(write_percentage: i32, transaction_size: usize, transaction_count: usize) -> u128 {
+    let db= DashMapDatabase::<String, usize>::new();
+
+    let mut transactions = Vec::<DashMapTableTransaction<String, usize>>::new();
+
+    for i in 0..transaction_count {
+        let mut modify = DashMapTableTransaction::new();
+        for j in 0..transaction_size {
+            if with_percentage_true(write_percentage) {
+                modify.set(format!("{}{}", i, j), j).unwrap();
+            } else {
+                modify.get(&format!("{}{}", i, j)).unwrap();
+            }
+        }
+        transactions.push(modify);
+    }
+    let mut test_table = db.empty_table();
+
+    let start: Instant = Instant::now();
+
+    for transaction in transactions {
+        test_table.execute(transaction);
+    }
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed  {:?}, write percentage: {}, transaction_size {}, transaction_count {}", duration, write_percentage, transaction_size, transaction_count);
+    duration.as_millis()
+}
 
 fn run_file_backup_test(write_percentage: i32, transaction_size: usize, transaction_count: usize) -> u128 {
     let mut db= FileStoreDatabase::<String, usize>::new();
@@ -163,6 +191,25 @@ fn run_write_percentage_no_backup() {
     write_to_csv(results, "run_write_percentage_no_backup.csv");
 }
 
+fn run_write_percentage_no_backup_dashmap() {
+    println!("Running run_write_percentage_no_backup_dashmap with dynamic transaction size");
+
+    const NUMBER_OF_OPERATIONS_POWER: u32 = 7;
+
+    let mut results = Vec::<(u128, i32, usize, usize)>::new();
+    for i in 2..NUMBER_OF_OPERATIONS_POWER  {
+            results.push((
+                run_no_backup_dashmap_test(10, usize::pow(10,i), 
+                usize::pow(10, NUMBER_OF_OPERATIONS_POWER - i)), i as i32,
+                usize::pow(10,i),
+                usize::pow(10, NUMBER_OF_OPERATIONS_POWER - i)
+            )
+            );
+    }
+
+    write_to_csv(results, "run_no_backup_dashmap_test.csv");
+}
+
 fn run_write_percentage_with_file_backup() {
     println!("Running run_write_percentage_with_file_backup with dynamic write percentage");
 
@@ -199,16 +246,15 @@ fn run_transaction_size_single_rocksdb() {
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let test_method = get_test_method(&args);
-    let backup_type = get_backup_type(&args);
-
-
+    // let test_method = get_test_method(&args);
+    // let backup_type = get_backup_type(&args);
 
     run_write_percentage_single_rocksdb();
     run_transaction_size_single_rocksdb();
     run_write_percentage_no_backup();
     run_write_percentage_with_file_backup();
+    run_write_percentage_no_backup_dashmap();
     
 }
